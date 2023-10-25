832379afc37cd3edf7b407dfb4d00ac2f83ae34b
Fabian Sommer <sommerauer.fabian@gmail.com>
2023-10-26 00:23:15 +0200

    Add conditional entropy utils

--- my_utils.py
+++ my_utils.py
@@ -55,3 +55,89 @@ def get_predictions(output_spikes, time_ranges, pattern_mapping):
         predictions[range_idx] = pattern_mapping[max_neuron]
 
     return predictions
+
+
+def get_joint_probabilities_over_time(output_spikes, time_ranges_per_pattern):
+    """Get joint probabilities of output neuron firing and pattern presentation over time"""
+
+    total_time = output_spikes.shape[0]
+    output_neuron_num = output_spikes.shape[-1]
+    pattern_num = len(time_ranges_per_pattern)
+
+    counts_over_time = np.zeros((total_time, pattern_num, output_neuron_num))
+
+    spike_times = np.where(output_spikes > 0)
+
+    for pattern_idx, time_ranges in enumerate(time_ranges_per_pattern):
+        time_range = np.stack(time_ranges, axis=0)
+        in_range = np.any((spike_times[0][:, None] <= time_range[:, 1]) & (spike_times[0][:, None] >= time_range[:, 0]),
+                          axis=-1)
+
+        counts_over_time[spike_times[0][in_range], pattern_idx, spike_times[-1][in_range]] = 1.
+
+    cumulative_counts = np.cumsum(counts_over_time, axis=0)
+
+    # avoid division by zero (assume "worst case" where no pattern is presented)
+    cumulative_counts[cumulative_counts == 0] = 1.
+
+    totals = np.sum(np.sum(cumulative_counts, axis=-1, keepdims=True), axis=-2, keepdims=True)
+
+    probabilities = cumulative_counts / totals
+
+    return probabilities
+
+
+# ensure documentation mentions shapes of numpy arguments
+
+def normalized_conditional_cross_entropy_paper(joint_probabilities):
+    """Compute normalized conditional cross entropy H(k|Z) / H(k,Z) based on joint probabilities
+    This is the normalization scheme described in the paper (https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1003037).
+    However, this metric is more difficult to interpret as the maximum value is not 1.0.
+
+    Args:
+        joint_probabilities: joint probabilities of pattern presentation and output neuron firing [shape (time, pattern, neuron) or (pattern, neuron)]
+    """
+
+    # compute conditional probabilities
+    conditional_probabilities = joint_probabilities / np.sum(joint_probabilities, axis=-1, keepdims=True)
+
+    # avoid division by zero (assume "worst case" where no pattern is presented)
+    conditional_probabilities[conditional_probabilities == 0] = 1.  # todo: check if this is correct
+
+    # compute cross entropy
+    cross_entropy = -np.sum(np.sum(joint_probabilities * np.log(joint_probabilities), axis=-1), axis=-2)
+    conditional_cross_entropy = -np.sum(np.sum(joint_probabilities * np.log(conditional_probabilities), axis=-1),
+                                        axis=-2)
+
+    # normalize
+    normalized_cond_cross_entropy = conditional_cross_entropy / cross_entropy
+
+    return normalized_cond_cross_entropy
+
+
+def normalized_conditional_cross_entropy(joint_probabilities):
+    """Compute normalized conditional cross entropy H(k|Z) / H(k) based on joint probabilities
+    We use a different normalization scheme than described in the paper (https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1003037).
+    In our case the normalized value is always within [0,1]. 1 if k and Z are independent, 0 if Z perfectly predicts k.
+
+    Args:
+        joint_probabilities: joint probabilities of pattern presentation and output neuron firing [shape (time, pattern, neuron) or (pattern, neuron)]
+    """
+
+    # compute conditional probabilities
+    conditional_probabilities = joint_probabilities / np.sum(joint_probabilities, axis=-1, keepdims=True)
+
+    # avoid division by zero (assume "worst case" where no pattern is presented)
+    conditional_probabilities[conditional_probabilities == 0] = 1.  # todo: check if this is correct
+
+    pattern_probabilities = np.sum(joint_probabilities, axis=-1)
+
+    # compute cross entropy
+    conditional_cross_entropy = -np.sum(np.sum(joint_probabilities * np.log(conditional_probabilities), axis=-1),
+                                        axis=-1)
+    pattern_cross_entropy = -np.sum(pattern_probabilities * np.log(pattern_probabilities), axis=-1)
+
+    # normalize
+    normalized_cond_cross_entropy = conditional_cross_entropy / pattern_cross_entropy
+
+    return normalized_cond_cross_entropy
