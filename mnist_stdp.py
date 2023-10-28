Improve mnist_test; Fix numerical instability

mnist_stdp.py
@@ -7,13 +7,14 @@ import torchmetrics
 
 import custom_stdp
 from my_spike_modules import *
-from my_utils import spike_in_range, get_neuron_pattern_mapping, get_predictions
+from my_utils import spike_in_range, get_neuron_pattern_mapping, get_predictions, get_joint_probabilities_over_time, \
+    normalized_conditional_cross_entropy, normalized_conditional_cross_entropy_paper
 from my_plot_utils import raster_plot, raster_plot_multi_color, raster_plot_multi_color_per_train
 
 # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-# Set seed
-seed = 44
+# Set seed (todo: test with different seeds)
+seed = 45
 random.seed(seed)
 _ = torch.manual_seed(seed)
 np.random.seed(seed)
@@ -22,13 +23,20 @@ np.random.seed(seed)
 batch_size = 10
 data_path = '/tmp/data/mnist'
 
+class ToBinaryTransform(object):
+    def __init__(self, thresh):
+        self.thresh = thresh
+
+    def __call__(self, data: Tensor):
+        return (data > self.thresh).to(data.dtype)
+
 # Data transforms
 transform = transforms.Compose([
     transforms.Resize((28, 28)),
     transforms.Grayscale(),
     transforms.ToTensor(),
     transforms.Normalize((0,), (1,)),
-    # ToBinaryTransform(0.5)
+    ToBinaryTransform(0.5)  # todo: test without this
 ])
 
 # Load data
@@ -38,25 +46,24 @@ mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=tra
 train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True)
 
-# filter data to subset of classes or subset of samples per class
+distinct_targets = mnist_train.targets.unique().cpu().numpy()
 
 
 # Model config
 _, width, height = mnist_train.data.shape
 binary_input_variable_cnt = width * height
 input_neurons = binary_input_variable_cnt * 2
-output_neurons = 3
+output_neurons = 100
 
 dt = 0.001
-sigma = 0.01
+sigma = 0.005
 
 presentation_duration = 0.04
 delay = 0.01
 
 # todo: experiment with different rates (maybe different rates for train and test as well)
 input_encoding_rate = 40
-input_encoding_inactive_rate = 5
-
+input_encoding_inactive_rate = 0
 
 # Model setup
 train_encoder = SpikePopulationGroupBatchToTimeEncoder(presentation_duration,
@@ -67,13 +74,12 @@ test_encoder = SpikePopulationGroupBatchToTimeEncoder(presentation_duration,
                                                       input_encoding_rate, input_encoding_inactive_rate,
                                                       delay, dt)
 
-stdp_module = custom_stdp.BayesianSTDPClassic(output_neurons, c=1, base_mu=1, base_mu_bias=0.5, collect_history=True)
+stdp_module = custom_stdp.BayesianSTDPClassic(output_neurons, c=1, base_mu=0.5, base_mu_bias=0.1, collect_history=True)
 # stdp_module = custom_stdp.BayesianSTDPAdaptive(input_neurons, output_neurons, c=1, collect_history=True)  #todo: get this to work
 
 inhibition_process = OUInhibitionProcess(inhibition_increase=3000, inhibition_rest=0, inhibition_tau=0.005,
                                          noise_rest=0, noise_tau=0.005, noise_sigma=50, dt=dt)
-output_cell = StochasticOutputNeuronCell(
-    inhibition_process=inhibition_process, dt=dt)
+output_cell = StochasticOutputNeuronCell(inhibition_process=inhibition_process, dt=dt, collect_rates=True)
 
 model = BayesianSTDPModel(input_neurons, output_neurons, BinaryTimedPSP(sigma, dt),
                           output_neuron_cell=output_cell,
@@ -81,11 +87,10 @@ model = BayesianSTDPModel(input_neurons, output_neurons, BinaryTimedPSP(sigma, d
 
 # Model initialization
 # todo: experiment with different initializations
-weight_init = 1
-bias_init = 2
-model.linear.weight.data.fill_(weight_init)
-model.linear.bias.data.fill_(bias_init)
-
+#weight_init = 1
+#bias_init = 2
+#model.linear.weight.data.fill_(weight_init)
+#model.linear.bias.data.fill_(bias_init)
 
 # Training config
 num_epochs = 1  # run for 1 epoch - each data sample is seen only once
@@ -94,47 +99,45 @@ loss_hist = []  # record loss over iterations
 acc_hist = []  # record accuracy over iterations
 
 state = None
-total_train_output_spikes = None
-total_time_ranges = None
+total_train_output_spikes = []
+total_time_ranges = [[] for _ in range(distinct_targets.shape[0])]
+joint_probs_hist = []  # todo: might not be needed
+cross_entropy_hist = []
+cross_entropy_paper_hist = []
 
 # training loop
+offset = 0
 for epoch in range(num_epochs):
     for i, (data, targets) in enumerate(iter(train_loader)):
+        data = rearrange(data, 'b c w h -> b (c w h)')
         input_spikes = train_encoder(data)
 
-        time_ranges = train_encoder.get_time_ranges_for_patterns(targets)
-        time_ranges_ungrouped = test_encoder.get_time_ranges(len(targets))
+        time_ranges = train_encoder.get_time_ranges_for_patterns(targets,
+                                                                 distinct_pattern_count=distinct_targets.shape[0],
+                                                                 offset=offset)
+        time_ranges_ungrouped = test_encoder.get_time_ranges(targets.shape[0], offset=offset)
 
         output_spikes, state = model(input_spikes, state=state, train=True)
 
-        # todo: collect and combine relevant metrics here (afterwards metric trackers can be reset)
+        output_spikes_np = output_spikes.cpu().numpy()
 
-        loss_val = loss_fn(output, targets)
-        loss_hist.append(loss_val.item())
+        total_train_output_spikes.extend(output_spikes_np)
+        for idx, time_range in enumerate(time_ranges):
+            total_time_ranges[idx].extend(time_range)
 
-        if i % 25 == 0:
-            print(f"Epoch {epoch}, Iteration {i} \nTrain Loss: {loss_val.item():.2f}")
+        joint_probs = get_joint_probabilities_over_time(np.array(total_train_output_spikes), total_time_ranges)
+        joint_probs_hist = joint_probs
 
-            # check accuracy on a single batch
-            acc = np.mean(np.argmax(output.detach().cpu().numpy(), axis=1) == targets.cpu().numpy())
-            acc_hist.append(acc)
-            print(f"Accuracy: {acc * 100:.2f}%\n")
+        cond_cross_entropy = normalized_conditional_cross_entropy(joint_probs)
+        cross_entropy_hist = cond_cross_entropy
 
-neuron_mapping = get_neuron_pattern_mapping(total_train_output_spikes.cpu().numpy(), total_time_ranges)
+        cond_cross_entropy_paper = normalized_conditional_cross_entropy_paper(joint_probs)
+        cross_entropy_paper_hist = cond_cross_entropy_paper
 
-total_acc = 0
-for i, (data, targets) in enumerate(iter(test_loader)):
-    input_spikes = test_encoder(data)
-    # todo: we reset the state for every batch here, should we?
-    output_spikes, _ = model(input_spikes, state=None, train=False)
+        offset += data.shape[0]
 
-    total_acc += np.mean(np.argmax(output.detach().cpu().numpy(), axis=1) == targets.cpu().numpy())
-
-print(f"Test Accuracy: {total_acc / len(test_loader) * 100:.2f}%")
-
-
-# TODO: detailed evaluation; plot weights; plot bias; plot firing rates; plot stdp; plot log probabilities; plot conditional crossentropy;
-# TODO: plot accuracy over training based on final pattern mapping
+        if i % 10 == 0:
+            print(f"Epoch {epoch}, Iteration {i} \nTrain Loss: {cond_cross_entropy[-1]:.2f}")
 
 
 torch.save(model.state_dict(), "trained_mnist_bayesian_stdp_model.pth")
@@ -142,25 +145,51 @@ torch.save(model.state_dict(), "trained_mnist_bayesian_stdp_model.pth")
 # model.load_state_dict(torch.load("trained_bayesian_stdp_model.pth"))
 
 
+neuron_mapping = get_neuron_pattern_mapping(total_train_output_spikes, total_time_ranges)
+
+
 output_cell.rate_tracker.is_active = True
 output_cell.rate_tracker.reset()
 model.state_metric.is_active = True
 model.state_metric.reset()
 
-input_spikes = test_encoder(test_data)
-output_spikes, _ = model(input_spikes, train=False)
+# test loop
+total_acc = 0
+for i, (data, targets) in enumerate(iter(test_loader)):
+    data = rearrange(data, 'b c w h -> b (c w h)')
+    input_spikes = test_encoder(data)
 
-# todo: this should be done on the train data (maybe only last few patterns)
-neuron_mapping = get_neuron_pattern_mapping(output_spikes.cpu().numpy(), test_time_ranges)
+    targets_np = targets.cpu().numpy()
+    time_ranges = test_encoder.get_time_ranges_for_patterns(targets_np, distinct_pattern_count=distinct_targets.shape[0])
+    time_ranges_ungrouped = test_encoder.get_time_ranges(targets_np.shape[0])
 
-print(input_spikes)
-print(output_spikes)
+    # todo: we reset the state for every batch here, should we?
+    output_spikes, _ = model(input_spikes, state=None, train=False)
+
+    preds = get_predictions(output_spikes, time_ranges, neuron_mapping)
+    total_acc += np.mean(preds == targets_np)
+
+print(f"Test Accuracy: {total_acc / len(test_loader) * 100:.4f}%")
+
+
+# TODO: detailed evaluation; plot weights; plot bias; plot firing rates; plot stdp; plot log probabilities; plot conditional crossentropy;
+# TODO: plot accuracy over training based on final pattern mapping
+
+plt.plot(cross_entropy_hist)
+plt.plot(cross_entropy_paper_hist)
+plt.title('Training')
+plt.xlabel('Time Step')
+plt.ylabel('Normalized Conditional Crossentropy')
+plt.ylim([0, 1])
+plt.xticks(np.arange(0, len(cross_entropy_hist), 100./dt),
+           [str(x) + 's' for x in np.arange(0, len(cross_entropy_hist) * dt, 100)])
+plt.show()
 
 model.state_metric.plot()
 
-group_colors = ("tab:orange", "tab:green", "tab:blue")
-allowed_colors = [[idx, ] for idx in
-                  neuron_mapping]
+cmap = plt.get_cmap("tab10")
+group_colors = [cmap(i) for i in range(distinct_targets.shape[0])]
+allowed_colors = [[idx, ] for idx in neuron_mapping]
 
 # Plot inputs
 plt.figure(figsize=(10, 7))
@@ -169,7 +198,7 @@ plt.subplot(3, 1, 1)
 first_ax = plt.gca()
 spikes = [input_spikes[:, i].cpu().numpy() for i in range(input_spikes.shape[1])]
 # raster_plot(plt.gca(), spikes)
-raster_plot_multi_color(plt.gca(), spikes, test_time_ranges, group_colors)
+raster_plot_multi_color(plt.gca(), spikes, time_ranges, group_colors)
 plt.title('Input Spikes')
 plt.xlabel('Time Step')
 plt.ylabel('Neuron')
@@ -178,7 +207,7 @@ plt.ylabel('Neuron')
 plt.subplot(3, 1, 2)
 plt.gca().sharex(first_ax)
 spikes = [output_spikes[:, i].cpu().numpy() for i in range(output_spikes.shape[1])]
-raster_plot_multi_color(plt.gca(), spikes, test_time_ranges, group_colors, default_color='black',
+raster_plot_multi_color(plt.gca(), spikes, time_ranges, group_colors, default_color='black',
                         allowed_colors_per_train=allowed_colors)
 # raster_plot_multi_color(spikes, plt.gca(), get_color_picker(("red", "green", "blue"), test_data_time_ranges))
 plt.title('Output Spikes')
@@ -196,10 +225,6 @@ plt.show()
 print(model.linear.weight)
 print(model.linear.bias)
 
-preds = get_predictions(output_spikes, test_time_ranges_ungrouped, neuron_mapping)
-# todo: test if below crashes on missing predictions
-acc = torchmetrics.functional.accuracy(torch.tensor(preds), test_pattern_order, task="multiclass", num_classes=3)
-print(acc)
 
 stdp_module.learning_rates_tracker.plot()
 

my_spike_modules.py
@@ -47,18 +47,19 @@ class SpikePopulationGroupBatchToTimeEncoder(nn.Module):
         combined = rearrange(padded, 't b ... i n -> (b t) ... (i n)')
         return combined
 
-    def get_time_ranges(self, pattern_count, epsilon=1e-5):
+    def get_time_ranges(self, pattern_count, epsilon=1e-5, offset=0):
         total_len = self.base_encoder.seq_length + self.delay_shift
         # todo: check with epsilon
-        time_ranges = [(i * total_len - epsilon, i * total_len + total_len - epsilon)
+        time_ranges = [((i+offset) * total_len - epsilon, (i+offset) * total_len + total_len - epsilon)
                        for i in range(pattern_count)]
 
         return time_ranges
 
-    def get_time_ranges_for_patterns(self, pattern_order, epsilon=1e-5):
-        time_ranges = self.get_time_ranges(len(pattern_order), epsilon)
+    def get_time_ranges_for_patterns(self, pattern_order, distinct_pattern_count=None, epsilon=1e-5, offset=0):
+        time_ranges = self.get_time_ranges(len(pattern_order), epsilon, offset)
 
-        distinct_pattern_count = len(np.unique(pattern_order))
+        if distinct_pattern_count is None:
+            distinct_pattern_count = len(np.unique(pattern_order))
         grouped_time_ranges = [[] for _ in range(distinct_pattern_count)]
         for index, time_range in zip(pattern_order, time_ranges):
             grouped_time_ranges[index].append(time_range)
@@ -151,10 +152,10 @@ class StochasticOutputNeuronCell(nn.Module):
         inhibition, noise = inhibition_state
 
         log_rates = inputs - inhibition + noise
-        input_rates = torch.exp(inputs)
+        relative_input_rates = torch.exp(inputs - torch.logsumexp(inputs, dim=-1, keepdim=True))
 
         # collect rates for plotting
-        self.rate_tracker.update(input_rates, log_rates)
+        self.rate_tracker.update(relative_input_rates, log_rates)
 
         # more numerically stable to utilize log
         log_total_rate = torch.logsumexp(log_rates, -1, keepdim=True)
@@ -169,10 +170,10 @@ class StochasticOutputNeuronCell(nn.Module):
 
         # here we only have to deal with input_rates as inhibition + noise cancels out
         # (makes process more numerically stable)
-        rel_probs = input_rates / torch.sum(input_rates, dim=-1, keepdim=True)
-        spike_location = efficient_multinomial(rel_probs)
+        # rel_probs = input_rates / torch.sum(input_rates, dim=-1, keepdim=True)  (should already be normalized due to logsumexp)
+        spike_location = efficient_multinomial(relative_input_rates)
 
-        out_spike_locations = F.one_hot(spike_location, num_classes=input_rates.shape[-1])
+        out_spike_locations = F.one_hot(spike_location, num_classes=relative_input_rates.shape[-1])
 
         # ensures that only one output neuron can fire at a time
         out_spikes = (out_spike_locations * spike_occurred).to(dtype=inputs.dtype)

my_utils.py
@@ -74,6 +74,8 @@ def get_joint_probabilities_over_time(output_spikes, time_ranges_per_pattern):
     spike_times = np.where(output_spikes > 0)
 
     for pattern_idx, time_ranges in enumerate(time_ranges_per_pattern):
+        if len(time_ranges) == 0:
+            continue
         time_range = np.stack(time_ranges, axis=0)
         in_range = np.any((spike_times[0][:, None] <= time_range[:, 1]) & (spike_times[0][:, None] >= time_range[:, 0]),
                           axis=-1)
@@ -116,10 +118,10 @@ def get_joint_probabilities_over_time_for_rates(relative_firing_rates, time_rang
             axis=-1)
 
         joint_probabilities_over_time[potential_spike_times[in_range], pattern_idx, :] = relative_firing_rates[
-                                                                              potential_spike_times[in_range], :]
+                                                                                         potential_spike_times[
+                                                                                             in_range], :]
 
     return joint_probabilities_over_time
-
 
 
 def normalized_conditional_cross_entropy_paper(joint_probabilities):
@@ -213,15 +215,15 @@ def get_input_likelihood(weights, biases, input_psp, input_groups, c=1.):
     group_normalized_single_input_log_likelihoods = np.log(single_input_likelihoods
                                                            / grouped_sum(single_input_likelihoods, input_groups))
 
-    input_log_likelihoods = np.sum(input_psp[..., None, :] * group_normalized_single_input_log_likelihoods[None, :, :], axis=-1)
+    input_log_likelihoods = np.sum(input_psp[..., None, :] * group_normalized_single_input_log_likelihoods[None, :, :],
+                                   axis=-1)
 
     total_input_likelihood = np.sum(np.exp(input_log_likelihoods) * normalized_priors, axis=-1)
 
     return total_input_likelihood
 
 
-
-#todo
+# todo
 # joint = get_joint_probabilities_over_time_for_rates(probs, train_time_ranges)
 # plt.plot(normalized_conditional_cross_entropy(moving_avg(joint, 500, 0)))
 def moving_avg(arr, len, axis):