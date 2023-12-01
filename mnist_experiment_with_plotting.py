497c2d388b54679edc50a040a493f8615de03406
Fabian Sommer <sommerauer.fabian@gmail.com>
2023-12-01 14:32:03 +0100

    Add evaluations and collect relevant figures

--- mnist_experiment_with_plotting.py
+++ mnist_experiment_with_plotting.py
@@ -14,10 +14,10 @@ from my_spike_modules import InhibitionArgs, NoiseArgs, LogFiringRateCalculation
     OutputBackgroundOscillationArgs, InputBackgroundOscillationArgs
 from my_utils import set_seed, reorder_dataset_by_targets, FlattenTransform, ToBinaryTransform
 from train_test_loop import ModelConfig, EncoderConfig, STDPConfig, OutputCellConfig, TrainConfig, train_model, \
-    TestConfig, test_model, STDPAdaptiveConfig
+    TestConfig, test_model, STDPAdaptiveConfig, STDPClassicConfig
 
 # Experiment name
-experiment_name = "adaptive_non_binarized_100Hz_0Hz_full"
+experiment_name = "classic_non_binarized_100Hz_0Hz_full"
 
 # Set seed
 seed = 85453
@@ -70,7 +70,7 @@ data_count = mnist_train.data.shape[0]
 input_osc_args = InputBackgroundOscillationArgs(0.5, 0.5, 20, -torch.pi / 2)
 output_osc_args = OutputBackgroundOscillationArgs(50, 20, -torch.pi / 2)
 
-inhibition_args = InhibitionArgs(2000, 50, 2e-3)
+inhibition_args = InhibitionArgs(2000, 50, 5e-3)
 noise_args = NoiseArgs(0, 5e-3, 50)
 
 model_config = ModelConfig(
@@ -89,8 +89,8 @@ model_config = ModelConfig(
     stdp_config=STDPConfig(
         c=1.,
         time_batch_size=10,
-        method=STDPAdaptiveConfig(base_mu=5e-1, base_mu_bias=5e-1)
-        # method=STDPClassicConfig(base_mu=1., base_mu_bias=1.)
+        # method=STDPAdaptiveConfig(base_mu=5e-1, base_mu_bias=5e-1)
+        method=STDPClassicConfig(base_mu=1., base_mu_bias=1.)
     ),
     output_cell_config=OutputCellConfig(
         inhibition_args=inhibition_args,
